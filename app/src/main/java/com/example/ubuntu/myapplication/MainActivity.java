e7280278043428c9c7cd96150fe506165d5857f5
ubuntu <1312655689@qq.com>
2018-05-27 23:07:34 +0800

    201805272314

--- app/src/main/java/com/example/ubuntu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ubuntu/myapplication/MainActivity.java
@@ -14,12 +14,15 @@ public class MainActivity extends AppCompatActivity {
     int[] IMAGES = {R.drawable.erina1, R.drawable.erina2, R.drawable.erina3, R.drawable.erina4,
             R.drawable.erina5, R.drawable.erina6, R.drawable.erina7, R.drawable.erina8,
             R.drawable.erina9, R.drawable.erina10, R.drawable.erina11, R.drawable.erina12,
-            R.drawable.erina13, R.drawable.erina14};
+            R.drawable.erina13, R.drawable.erina14, R.drawable.erina15, R.drawable.erina16,
+            R.drawable.erina17, R.drawable.erina18, R.drawable.erina19, R.drawable.erina20};
     String[] NAMES = {"Erina1", "Erina2", "Erina3", "Erina4", "Erina5", "Erina6", "Erina7",
             "Erina8", "Erina9", "Erina10", "Erina11", "Erina12", "Erina13", "Erina14", "Erina15",
-    "Erina15"};
+    "Erina16", "Erina17", "Erina18", "Erina19", "Erina20"};
 
     String[] DECRIPTIONS = {"Japanese idol Mano Erina", "Japanese idol Mano Erina",
+            "Japanese idol Mano Erina", "Japanese idol Mano Erina", "Japanese idol Mano Erina",
+            "Japanese idol Mano Erina", "Japanese idol Mano Erina", "Japanese idol Mano Erina",
             "Japanese idol Mano Erina", "Japanese idol Mano Erina", "Japanese idol Mano Erina",
             "Japanese idol Mano Erina", "Japanese idol Mano Erina", "Japanese idol Mano Erina",
             "Japanese idol Mano Erina", "Japanese idol Mano Erina", "Japanese idol Mano Erina",
@@ -39,31 +42,42 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
+
             return IMAGES.length;
         }
 
         @Override
         public Object getItem(int i) {
+
             return null;
         }
 
         @Override
         public long getItemId(int i) {
+
             return 0;
         }
 
         @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            view = getLayoutInflater().inflate(R.layout.customlayout, null);
-            ImageView imageView = (ImageView)view.findViewById(R.id.imageView);
-            TextView textView_name = (TextView)view.findViewById(R.id.textView_name);
-            TextView textView_description = (TextView)view.findViewById(R.id.textView_description);
+        public View getView(int i, View convertView, ViewGroup viewGroup) {
+            View view;
+            convertView = getLayoutInflater().inflate(R.layout.customlayout, null);
+            ImageView imageView = (ImageView)convertView.findViewById(R.id.imageView);
+            TextView textView_name = (TextView)convertView.findViewById(R.id.textView_name);
+            TextView textView_description = (TextView)convertView.findViewById(R.id.textView_description);
 
             imageView.setImageResource(IMAGES[i]);
             textView_name.setText(NAMES[i]);
             textView_description.setText(DECRIPTIONS[i]);
 
+            if (convertView == null){
+                view = View.inflate(getApplicationContext(), R.layout.customlayout, null);
+            } else
+                {
+                view = convertView;
+            }
             return view;
         }
+        
     }
 }
